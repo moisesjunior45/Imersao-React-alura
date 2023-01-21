ff0a4a4dcf280fa4d21e08117ebf2c491b272c58
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2023-01-21 03:49:14 -0300

    Update index.js

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -3,19 +3,45 @@ import { StyledRegisterVideo } from "./styles";
 
 export default function RegisterVideo() {
     const [formVisivel, setFormVisivel] = React.useState(false);
+    const [values, setValues] = React.useState({ titulo: "", url: "" });
+
     return (
         <StyledRegisterVideo>
-            <button className="add-video">
+            <button className="add-video" onClick={() => setFormVisivel(true)}>
                 +
             </button>
             {formVisivel ? (
-                <form>
+                <form onSubmit={(evento) => {
+                    evento.preventDefault();
+                }}>
                     <div>
-                        <button className="close-modal">
+                        <button className="close-modal" onClick={() => setFormVisivel(false)}>
                             X
                         </button>
-                        <input placeholder="Titulo do video" />
-                        <input placeholder="URL do video" />
+                        <input
+                            placeholder="Titulo do video"
+                            value={values.titulo}
+                            onChange={(evento) => {
+                                const value = evento.target.value;
+                                console.log(value)
+                                setValues({
+                                    ...values,
+                                    titulo: value,
+                                });
+                            }}
+                        />
+                        <input
+                            placeholder="URL do video"
+                            value={values.url}
+                            onChange={(evento) => {
+                                const value = evento.target.value;
+                                console.log(value)
+                                setValues({
+                                    ...values,
+                                    url: value,
+                                });
+                            }}
+                        />
                         <button type="submit">
                             Cadastrar
                         </button>

15a68d75a7e6d85c122d1658612605fa1aea9120
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2023-01-22 20:51:59 -0300

    Editando arquivos

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,53 +1,66 @@
 import React from "react";
 import { StyledRegisterVideo } from "./styles";
 
+// Custom Hook
+function useForm(propsDoForm) {
+    const [values, setValues] = React.useState(propsDoForm.initialValues);
+    return {
+        values,
+        handleChange: (evento) => {
+            const value = evento.target.value;
+            const name = evento.target.name;
+            setValues({
+                ...values,
+                [name]: value,
+            });
+        },
+        clearForm() {
+            setValues({});
+        }
+    };
+}
+
 export default function RegisterVideo() {
+    const formCadastro = useForm({
+        initialValues: { titulo: "God of War", url: "https://youtube.." }
+    });
     const [formVisivel, setFormVisivel] = React.useState(false);
-    const [values, setValues] = React.useState({ titulo: "", url: "" });
 
     return (
         <StyledRegisterVideo>
             <button className="add-video" onClick={() => setFormVisivel(true)}>
                 +
             </button>
-            {formVisivel ? (
-                <form onSubmit={(evento) => {
-                    evento.preventDefault();
-                }}>
-                    <div>
-                        <button className="close-modal" onClick={() => setFormVisivel(false)}>
-                            X
-                        </button>
-                        <input
-                            placeholder="Titulo do video"
-                            value={values.titulo}
-                            onChange={(evento) => {
-                                const value = evento.target.value;
-                                console.log(value)
-                                setValues({
-                                    ...values,
-                                    titulo: value,
-                                });
-                            }}
-                        />
-                        <input
-                            placeholder="URL do video"
-                            value={values.url}
-                            onChange={(evento) => {
-                                const value = evento.target.value;
-                                console.log(value)
-                                setValues({
-                                    ...values,
-                                    url: value,
-                                });
-                            }}
-                        />
-                        <button type="submit">
-                            Cadastrar
-                        </button>
-                    </div>
-                </form>
-            )
+            {formVisivel
+                ? (
+                    <form onSubmit={(evento) => {
+                        evento.preventDefault();
+                        console.log(formCadastro.values);
+                        setFormVisivel(false);
+                        formCadastro.clearForm();
+                    }}>
+                        <div>
+                            <button type="button" className="close-modal" onClick={() => setFormVisivel(false)}>
+                                X
+                            </button>
+                            <input
+                                placeholder="Titulo do video"
+                                name="titulo"
+                                value={formCadastro.values.titulo}
+                                onChange={formCadastro.handleChange}
+                            />
+                            <input
+                                placeholder="URL do video"
+                                name="url"
+                                value={formCadastro.values.url}
+                                onChange={formCadastro.handleChange}
+                            />
+                            <button type="submit">
+                                Cadastrar
+                            </button>
+                        </div>
+                    </form>
+                )
                 : false}
         </StyledRegisterVideo>
     )
